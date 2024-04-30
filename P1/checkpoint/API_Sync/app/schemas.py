8826ca1970489ae8297f7f91d67fa35afbb490a1
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2024-04-30 09:09:42 -0300

    ADD auth

--- P1/checkpoint/API_Sync/app/schemas.py
+++ P1/checkpoint/API_Sync/app/schemas.py
@@ -1,14 +1,6 @@
 from pydantic import BaseModel
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str 
-
 class TaskBase(BaseModel):
     title: str
     description: str
