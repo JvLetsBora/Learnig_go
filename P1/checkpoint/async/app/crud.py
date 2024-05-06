ebcc88100edec3da0259303480e254dd249c8362
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2024-05-06 13:34:34 -0300

    ...

--- P1/checkpoint/async/app/crud.py
+++ P1/checkpoint/async/app/crud.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import Session
-
 from . import models, schemas
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from passlib.context import CryptContext
 brcypt_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
 
@@ -13,22 +13,28 @@ def create_token(user: schemas.TokenCreate) -> schemas.Token:
 
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+async def get_user(db: AsyncSession, user_id: int):
+    return (await db.scalars(select(models.User).where(models.User.id == user_id))).first()
 
-def get_task(db: Session, task_id: int):
-    return db.query(models.Task).filter(models.Task.id == task_id).first()
+async def get_task(db: AsyncSession, task_id: int):
+    return (await db.scalars(select(models.Task).where(models.Task.id == task_id))).first() 
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-
-    return db.query(models.User).offset(skip).limit(limit).all()
+async def get_user_by_email(db: AsyncSession, email: str):
+    return (await db.scalars(select(models.User).where(models.User.email == email))).first()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
+async def get_users(db: AsyncSession, skip: int = 0, limit: int = 100):
+    try:
+        results = (await db.scalars(select(models.User).offset(skip).limit(limit))).all()
+        users_list = [user.__dict__ for user in results]
+        return users_list
+    except:
+        return None
+    
+    
+
+
+async def create_user(db: AsyncSession, user: schemas.UserCreate):
     hashed_password = brcypt_context.hash(user.password)
     db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
@@ -37,39 +43,45 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user 
 
 
-def get_tasks(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Task).offset(skip).limit(limit).all()
+async def get_tasks(db: AsyncSession, skip: int = 0, limit: int = 100):
+    try:
+        results = (await db.scalars(select(models.Task).offset(skip).limit(limit))).all()
+        tasks_list = [task.__dict__ for task in results]
+        return tasks_list
+    except:
+        return None
+    
 
-def get_token(db: Session, token: schemas.Token, user: schemas.TokenCreate):
+async def get_token(db: AsyncSession, token: schemas.Token, user: schemas.TokenCreate):
     token = create_token(user)
     return token
 
 
-def create_user_task(db: Session, task: schemas.TaskCreate, user_id: int):
+async def create_user_task(db: AsyncSession, task: schemas.TaskCreate, user_id: int):
     db_task = models.Task(**task.dict(), owner_id=user_id)
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
     return db_task
 
-def delete_user(db: Session, user_id: int):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
+async def delete_user(db: AsyncSession, user_id: int):
+    user = (await db.scalars(select(models.User).where(models.User.id == user_id))).first()
     if user:
         db.delete(user)
         db.commit()
         return user
     return None
 
-def delete_task(db: Session, task_id: int):
-    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+async def delete_task(db: AsyncSession, task_id: int):
+    task = (await db.scalars(select(models.Task).where(models.Task.id == task_id))).first() 
     if task:
         db.delete(task)
         db.commit()
         return task
     return None
 
-def update_task(db: Session, task_id: int, task_data: schemas.TaskUpdate):
-    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+async def update_task(db: AsyncSession, task_id: int, task_data: schemas.TaskUpdate):
+    task = (await db.scalars(select(models.Task).where(models.Task.id == task_id))).first() 
     if task:
         for key, value in task_data.dict().items():
             setattr(task, key, value)
@@ -78,8 +90,8 @@ def update_task(db: Session, task_id: int, task_data: schemas.TaskUpdate):
         return task
     return None
 
-def update_user(db: Session, user_id: int, user_update: schemas.UserUpdate):
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+async def update_user(db: AsyncSession, user_id: int, user_update: schemas.UserUpdate):
+    db_user = (await db.scalars(select(models.User).where(models.User.id == user_id))).first()
     if db_user:
         for attr, value in user_update.dict().items():
             setattr(db_user, attr, value)
--- P1/checkpoint/async/app/main.py
+++ P1/checkpoint/async/app/main.py
@@ -1,89 +1,106 @@
+import logging
+import sys
+from contextlib import asynccontextmanager
+
+import uvicorn
+from app.config import settings
+from app.databaseTeste import sessionmanager, get_db_session, AsyncSession
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-from . import crud, models, schemas, auth
-from .database import SessionLocal, engine
+from . import crud,  schemas
 
 
 
-models.Base.metadata.create_all(bind=engine)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Function that handles startup and shutdown events.
+    To understand more, read https://fastapi.tiangolo.com/advanced/events/
+    """
+    yield
+    if sessionmanager._engine is not None:
+        # Close the DB connection
+        await sessionmanager.close()
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-app = FastAPI()
-app.include_router(auth.router)
+app = FastAPI(lifespan=lifespan, title=settings.project_name, docs_url="/api/docs")
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(
-    user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+async def create_user(
+    user: schemas.UserCreate, db: AsyncSession = Depends(get_db_session)):
+    db_user = await crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+async def read_users(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db_session)):
+    users = await crud.get_users(db, skip=skip, limit=limit)
+    if users is None:
+        raise HTTPException(status_code=404, detail="User not found")
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+async def read_user(user_id: int, db: AsyncSession = Depends(get_db_session)):
+    db_user = await crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
 @app.post("/users/{user_id}/tasks/", response_model=schemas.Task)
-def create_task_for_user(
-    user_id: int, task: schemas.TaskCreate, db: Session = Depends(get_db)
+async def create_task_for_user(
+    user_id: int, task: schemas.TaskCreate, db: AsyncSession = Depends(get_db_session)
 ):
     return crud.create_user_task(db=db, task=task, user_id=user_id)
 
 
 @app.get("/tasks/", response_model=list[schemas.Task])
-def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    tasks = crud.get_tasks(db, skip=skip, limit=limit)
+async def read_tasks(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db_session)):
+    tasks = await crud.get_tasks(db, skip=skip, limit=limit)
     return tasks
 
 
 @app.delete("/users/{user_id}", response_model=schemas.User)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+async def delete_user(user_id: int, db: AsyncSession = Depends(get_db_session)):
+    db_user = await crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return crud.delete_user(db=db, user_id=user_id)
 
 @app.delete("/tasks/{task_id}", response_model=schemas.Task)
-def delete_task(task_id: int, db: Session = Depends(get_db)):
-    db_task = crud.get_task(db, task_id=task_id)
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db_session)):
+    db_task = await crud.get_task(db, task_id=task_id)
     if db_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return crud.delete_task(db=db, task_id=task_id)
 
 
 @app.put("/users/{user_id}", response_model=schemas.User)
-def update_user(user_id: int, user_update: schemas.UserUpdate, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+async def update_user(user_id: int, user_update: schemas.UserUpdate, db: AsyncSession = Depends(get_db_session)):
+    db_user = await crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return crud.update_user(db=db, user_id=user_id, user_update=user_update)
 
 
 @app.put("/tasks/{task_id}", response_model=schemas.Task)
-def update_task(task_id: int, task_update: schemas.TaskUpdate, db: Session = Depends(get_db)):
-    db_task = crud.get_task(db, task_id=task_id)
+async def update_task(task_id: int, task_update: schemas.TaskUpdate, db: AsyncSession = Depends(get_db_session)):
+    db_task = await crud.get_task(db, task_id=task_id)
     if db_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return crud.update_task(db=db, task_id=task_id, task_update=task_update)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
